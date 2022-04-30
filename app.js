204941554669c21d5f834c0551b9431ae514e5fd
王冬 <wangdong@wangdongdeMacBook-Air.local>
2022-04-30 10:41:20 +0800

    MOD 添加版本号

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public/dist/0.1.0')));
 
 
 
